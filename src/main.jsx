2ea7a69b585dc3de3f9166a1ede10dc61aaedc22
minion9 <agajjar93@gmail.com>
2025-01-07 04:35:18 +0530

    initialize Auth Login #2

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,8 @@ import App from "./App.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Auth0Provider
-      domain="dev-ky3brio60b4bngvy.eu.auth0.com"
-      clientId="MsiHKlkA3TU3k3qKtKUeVFPkCNpZeZl9"
+      domain={import.meta.env.VITE_AUTH0_DOMAIN}
+      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
       authorizationParams={{
         redirect_uri: window.location.origin,
       }}
