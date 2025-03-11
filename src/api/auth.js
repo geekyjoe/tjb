25b3c4d36ada5832d449accce01cdc4d5da29cc8
Joe <joepgo1@outlook.com>
2025-03-11 17:26:07 +0530

    Update Api Url

--- src/api/auth.js
+++ src/api/auth.js
@@ -2,7 +2,7 @@
 
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/api";
+const API_URL = "https://appi-csvt.onrender.com/api";
 
 // Create axios instance with default configs
 const apiClient = axios.create({
